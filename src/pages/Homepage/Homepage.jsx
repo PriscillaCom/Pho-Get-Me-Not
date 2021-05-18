094f4c567f7fa85602c4e72f0cec75fb84f55819
Priscilla <priscillamoc97@gmail.com>
2021-05-17 17:18:28 -0700

    Added Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import coverPhoto from '../../assets/pho5.jpg';
+
 import './Homepage.css';
 
 const Homepage = () => (
     <div className="homepage">
-        <h1>hi</h1>
+        <img className='cover' src={coverPhoto} alt="" />
     </div>
 );
 
