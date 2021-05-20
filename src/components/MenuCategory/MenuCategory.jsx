462b515478ae69dc11eb5d80296256e8ab587ae3
Priscilla <priscillamoc97@gmail.com>
2021-05-20 15:25:59 -0700

    Added the scroll to Id

--- src/components/MenuCategory/MenuCategory.jsx
+++ src/components/MenuCategory/MenuCategory.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './MenuCategory.css';
 
-
-const MenuCategory= ({title,imageUrl}) => (
+const MenuCategory= ({title,imageUrl}) => {
+    return(
     <div className="menuCategory">
         <div>
             <img className='image' src={imageUrl} alt='food'/>
         </div>
         <div>
-            <Link to={`/#${title}`}>{title.toUpperCase()} </Link>
+            <Link to={`/menu/#${title}`}>{title.toUpperCase()} </Link>
         </div>
     </div>
-);
+    );
+};
 
 export default MenuCategory;
--- src/components/MenuPreview/MenuPreview.jsx
+++ src/components/MenuPreview/MenuPreview.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 import MenuItems from '../MenuItems/MenuItems.jsx'
 import './MenuPreview.css';
 
-const MenuPreview = ({title, food}) => (
+const MenuPreview = ({title, food}) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if(location.hash){
+            let elem = document.getElementById(location.hash.slice(1));
+            elem 
+                ? elem.scrollIntoView({behavior: 'smooth'})
+                : window.scrollTo({top:0,left:0,behavior:"smooth"});
+        }
+        else
+            return window.scrollTo({top:0,left:0,behavior:"smooth"});
+    }, [location]);
+
+    return(
     <div className='menu_preview'>
-        <h1>{title.toUpperCase()}</h1>
+        <h1 id={title}>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
                 food.map(({id,...otherProps}) => (
@@ -13,6 +28,7 @@ const MenuPreview = ({title, food}) => (
             }
         </div>
     </div>
-);
+    );
+};
 
 export default MenuPreview
