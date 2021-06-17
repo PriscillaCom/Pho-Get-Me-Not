c6dbe4ae801f6872f48e9f785dce9d9ccbfe2cd8
Priscilla <priscillamoc97@gmail.com>
2021-06-17 16:13:05 -0700

    Added cart dropdown container

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ShopContext from '../../context/ShopContext';
 
+import './CartDropdown.css';
+
 const CartDropdown = () => {
     return(
         <ShopContext.Consumer>
             {context => (
-                <div>
+                <div className='cart-dropdown-container'>
                     {
                         context.cart.length ? 
                         <span>Howdy</span>
